09fd95b241994090e5ad6bda2a68a451b6525b62
aschramm23 <aschramm@23andme.com>
2015-07-26 22:05:34 -0700

    Add base 23andMe connect module with 2 intro steps and 1 default complete step

--- ResearchKit/Common/ORKOrderedTask+ORKPredefinedActiveTask.h
+++ ResearchKit/Common/ORKOrderedTask+ORKPredefinedActiveTask.h
@@ -681,6 +681,18 @@ NS_ASSUME_NONNULL_BEGIN
                                         trailType:(ORKTrailMakingTypeIdentifier)trailType
                                           options:(ORKPredefinedTaskOption)options;
 
+
+/**
+ Returns a predefined task that connects a participant with 23andMe
+ 
+ TODO: Finish writing the detailed documentation for this method
+ */
++ (ORKOrderedTask *)twentyThreeAndMeTaskWithIdentifier:(NSString *)identifier
+                                           partnerLogo:(NSString *)logoName
+                                          authClientId:(NSString *)clientId
+                                            authScopes:(NSString *)scopes
+                                       sharingOptional:(BOOL)sharingOptional;
+
 @end
 
 NS_ASSUME_NONNULL_END
