b9092e5bdaae61f261d538e021967872787cffe8
aschramm23 <aschramm@23andme.com>
2016-03-04 19:26:25 -0800

    Replace UIWebView with WebKit

--- ResearchKit/TwentyThreeAndMe/ORKTwentyThreeAndMeConnectStepViewController.m
+++ ResearchKit/TwentyThreeAndMe/ORKTwentyThreeAndMeConnectStepViewController.m
@@ -12,15 +12,11 @@
 #import "ORKSkin.h"
 #import "ORKTwentyThreeAndMeConnectResult.h"
 
-@interface ORKTwentyThreeAndMeConnectStepViewController ()<UIWebViewDelegate>
+@interface ORKTwentyThreeAndMeConnectStepViewController ()<WKNavigationDelegate>
 
-@property (nonatomic, strong) UIWebView *webView;
+@property (nonatomic, strong) WKWebView *webView;
 
 @property (strong, nonatomic) NSMutableData *receivedData;
-
-@property (nonatomic) BOOL authenticated;
-
-@property (nonatomic, strong) NSURLRequest *failedRequest;
 
 @property (nonatomic, strong) NSString *authToken;
 
@@ -36,47 +32,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-        
-    self.view.backgroundColor = ORKColor(ORKBackgroundColorKey);
     
-    _webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
-    
-    const CGFloat horizMargin = ORKStandardLeftMarginForTableViewCell(self.view);
-    _webView.backgroundColor = ORKColor(ORKBackgroundColorKey);
-    _webView.scrollView.backgroundColor = ORKColor(ORKBackgroundColorKey);
-    
-    _webView.clipsToBounds = NO;
-    _webView.scrollView.clipsToBounds = NO;
-    _webView.scrollView.scrollIndicatorInsets = (UIEdgeInsets){.left = -horizMargin, .right = -horizMargin};
-    _webView.opaque = NO; // If opaque is set to YES, _webView shows a black right margin during transition when modally presented. This is an artifact due to disabling clipsToBounds to be able to show the scroll indicator outside the view.
-    
-    [_webView setScalesPageToFit:YES];
-    
+    WKWebViewConfiguration *wkConfiguration = [[WKWebViewConfiguration alloc] init];
+    self.webView = [[WKWebView alloc] initWithFrame:self.view.frame configuration:wkConfiguration];
+    self.webView.navigationDelegate = self;
     NSURL *contentURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://api.product.researchkit.23andme.us/authorize/?redirect_uri=%@&client_id=%@&select_profile=true&scope=%@", [self connectStep].redirectURI, [self connectStep].clientId, [self connectStep].scopes]];
-    [_webView loadRequest:[NSURLRequest requestWithURL:contentURL]];
-    
-    _webView.delegate = self;
-    [self.view addSubview:_webView];
-    
-    _webView.translatesAutoresizingMaskIntoConstraints = NO;
-    [self setupConstraints];
+    NSURLRequest *nsRequest=[NSURLRequest requestWithURL:contentURL];
+    [self.webView loadRequest:nsRequest];
+    [self.view addSubview:self.webView];
 }
 
-- (void)setupConstraints {
-    NSMutableArray *constraints = [NSMutableArray new];
-    
-    NSDictionary *views = NSDictionaryOfVariableBindings(_webView);
-    const CGFloat horizMargin = ORKStandardLeftMarginForTableViewCell(self.view);
-    [constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-horizMargin-[_webView]-horizMargin-|"
-                                                                             options:(NSLayoutFormatOptions)0
-                                                                             metrics:@{ @"horizMargin": @(horizMargin) }
-                                                                               views:views]];
-    [constraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_webView]|"
-                                                                             options:(NSLayoutFormatOptions)0 metrics:nil
-                                                                               views:views]];
-    
-    [NSLayoutConstraint activateConstraints:constraints];
-}
+#pragma mark - ORKStepResult
 
 - (ORKStepResult *)result {
     ORKStepResult *sResult = [super result];
@@ -101,70 +67,57 @@
 
 #pragma mark - WebViewDelegate
 
-- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
-    BOOL result = self.authenticated;
-    if( ! self.authenticated ) {
-        self.failedRequest = request;
-        NSURLConnection *urlConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
-        [urlConnection start];
-        return result;
-    }
-    else {
-        if ([[[request URL] host] isEqualToString:@"localhost"]) {
-            // Extract oauth_verifier from URL query
-            NSString *authCode = nil;
-            NSString *errorCode = nil;
-            NSArray* urlParams = [[[request URL] query] componentsSeparatedByString:@"&"];
-            for (NSString* param in urlParams)
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
+{
+    NSURLRequest *request = navigationAction.request;
+    if ([[[request URL] host] isEqualToString:@"localhost"]) {
+        // Extract oauth_verifier from URL query
+        NSString *authCode = nil;
+        NSString *errorCode = nil;
+        NSArray* urlParams = [[[request URL] query] componentsSeparatedByString:@"&"];
+        for (NSString* param in urlParams)
+        {
+            NSArray* keyValue = [param componentsSeparatedByString:@"="];
+            NSString* key = [keyValue objectAtIndex:0];
+            if ([key isEqualToString:@"code"])
             {
-                NSArray* keyValue = [param componentsSeparatedByString:@"="];
-                NSString* key = [keyValue objectAtIndex:0];
-                if ([key isEqualToString:@"code"])
-                {
-                    authCode = [keyValue objectAtIndex:1];
-                    break;
-                }
-                else if([key isEqualToString:@"error"])
-                {
-                    errorCode = [keyValue objectAtIndex:1];
-                    break;
-                }
+                authCode = [keyValue objectAtIndex:1];
+                break;
             }
-            
-            if (authCode) {
-                NSString *data = [NSString stringWithFormat:@"client_id=%@&client_secret=%@&grant_type=authorization_code&code=%@&redirect_uri=%@&scope=%@", [self connectStep].clientId, [self connectStep].clientSecret, authCode, [self connectStep].redirectURI, [self connectStep].scopes];
-                NSString *tokenURL = @"https://api.product.researchkit.23andme.us/token/";
-                NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:tokenURL]];
-                [request setHTTPMethod:@"POST"];
-                [request setHTTPBody:[data dataUsingEncoding:NSUTF8StringEncoding]];
-                
-                self.receivedData = [[NSMutableData alloc] init];
-                NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
-                [theConnection start];
+            else if([key isEqualToString:@"error"])
+            {
+                errorCode = [keyValue objectAtIndex:1];
+                break;
             }
-            else if (errorCode && [errorCode isEqualToString:@"access_denied"]) {
-                [self goForward];
-            }
-            else {
-                // Unexpected Request. Drop on floor for now.
-            }
-            
-            return NO;
         }
+        
+        if (authCode) {
+            NSString *data = [NSString stringWithFormat:@"client_id=%@&client_secret=%@&grant_type=authorization_code&code=%@&redirect_uri=%@&scope=%@", [self connectStep].clientId, [self connectStep].clientSecret, authCode, [self connectStep].redirectURI, [self connectStep].scopes];
+            NSString *tokenURL = @"https://api.product.researchkit.23andme.us/token/";
+            NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:tokenURL]];
+            [request setHTTPMethod:@"POST"];
+            [request setHTTPBody:[data dataUsingEncoding:NSUTF8StringEncoding]];
+            
+            self.receivedData = [[NSMutableData alloc] init];
+            NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+            [theConnection start];
+        }
+        else if (errorCode && [errorCode isEqualToString:@"access_denied"]) {
+            [self goForward];
+        }
+        else {
+            // Unexpected Request. Drop on floor for now.
+        }
+        
+        decisionHandler(WKNavigationActionPolicyCancel);
     }
-    
-    return YES;
+    else
+    {
+        decisionHandler(WKNavigationActionPolicyAllow);
+    }
 }
 
-#pragma mark - NSURLConnectionDelegate
-
--(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)pResponse {
-    if( ! self.authenticated ) {
-        self.authenticated = YES;
-        [connection cancel];
-        [self.webView loadRequest:self.failedRequest];
-    }
-}
+#pragma mark - NSURLConnectionDelegate - Handles Code to Token Connection
 
 -(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
 {
@@ -182,8 +135,7 @@
 
 -(void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
-    if( self.authenticated &&
-        self.receivedData )
+    if( self.receivedData )
     {
         //NSLog( @"%@", self.receivedData );
         
