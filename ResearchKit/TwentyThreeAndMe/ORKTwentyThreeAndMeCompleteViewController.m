49483b4e3b39de752eb870a966987f32e4e0f7f7
aschramm23 <aschramm@23andme.com>
2016-03-07 16:20:51 -0800

    Add copyright, contact/info buttons, and comments

--- ResearchKit/TwentyThreeAndMe/ORKTwentyThreeAndMeCompleteViewController.m
+++ ResearchKit/TwentyThreeAndMe/ORKTwentyThreeAndMeCompleteViewController.m
@@ -1,10 +1,32 @@
-//
-//  ORKTwentyThreeAndMeCompleteViewController.m
-//  ResearchKit
-//
-//  Created by Andrew Schramm on 1/26/16.
-//  Copyright © 2016 researchkit.org. All rights reserved.
-//
+/*
+ Copyright (c) 2016, 23andMe, Inc. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+ 
+ 1.  Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ 2.  Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation and/or
+ other materials provided with the distribution.
+ 
+ 3.  Neither the name of the copyright holder(s) nor the names of any contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission. No license is granted to the trademarks of
+ the copyright holders even if such marks are included in this software.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "ORKTwentyThreeAndMeCompleteViewController.h"
 #import "ORKTwentyThreeAndMeCompleteStep.h"
@@ -81,28 +103,30 @@
 
 - (void)showSuccessExistingViewController
 {
-    [self.view addSubview:self.successExistingViewController.view];
     [self addChildViewController:self.successExistingViewController];
+    [self.view addSubview:self.successExistingViewController.view];
     [self.successExistingViewController didMoveToParentViewController:self];
 }
 
 - (void)hideSuccessExistingViewController
 {
-    [self.successExistingViewController removeFromParentViewController];
+    [self.successExistingViewController willMoveToParentViewController:nil];
     [self.successExistingViewController.view removeFromSuperview];
+    [self.successExistingViewController removeFromParentViewController];
 }
 
 - (void)showFailureViewController
 {
-    [self.view addSubview:self.failureViewController.view];
     [self addChildViewController:self.failureViewController];
+    [self.view addSubview:self.failureViewController.view];
     [self.failureViewController didMoveToParentViewController:self];
 }
 
 - (void)hideFailureViewController
 {
-    [self.failureViewController removeFromParentViewController];
+    [self.failureViewController willMoveToParentViewController:nil];
     [self.failureViewController.view removeFromSuperview];
+    [self.failureViewController removeFromParentViewController];
 }
 
 @end
--- ResearchKit/TwentyThreeAndMe/ORKTwentyThreeAndMeFailureViewController.m
+++ ResearchKit/TwentyThreeAndMe/ORKTwentyThreeAndMeFailureViewController.m
@@ -1,10 +1,32 @@
-//
-//  ORKTwentyThreeAndMeFailureViewController.m
-//  ResearchKit
-//
-//  Created by Andrew Schramm on 1/21/16.
-//  Copyright © 2016 researchkit.org. All rights reserved.
-//
+/*
+ Copyright (c) 2016, 23andMe, Inc. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+ 
+ 1.  Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ 2.  Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation and/or
+ other materials provided with the distribution.
+ 
+ 3.  Neither the name of the copyright holder(s) nor the names of any contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission. No license is granted to the trademarks of
+ the copyright holders even if such marks are included in this software.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "ORKTwentyThreeAndMeFailureViewController.h"
 #import "UIButton+T23.h"
@@ -28,6 +50,14 @@
 
 - (void)declineButtonPressed:(UIButton *)sender {
     [self.delegate declineButtonPressed];
+}
+
+- (void)contactStudyButtonPressed:(UIButton *)sender {
+    NSString *contactStudyByMail = [NSString stringWithFormat:@"mailto:?to=%@&subject=%@&body=%@",
+                                    [self.studyContactEmail stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding],
+                                    [self.studyDisplayName stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding],
+                                    @""];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:contactStudyByMail]];
 }
 
 - (void)setupAppearance {
@@ -137,13 +167,45 @@
                                                                 attribute:NSLayoutAttributeTrailing
                                                                multiplier:1.0
                                                                  constant:-15.0]];
-    [textContentView addConstraint:[NSLayoutConstraint constraintWithItem:descriptionLabel
+    
+    UIButton *contactStudyButton = [[UIButton alloc] init];
+    NSString *contactStudyText = [NSString stringWithFormat:@"Contact %@", self.studyDisplayName];
+    [contactStudyButton addTarget:self action:@selector(contactStudyButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [contactStudyButton setTitle:contactStudyText forState:UIControlStateNormal];
+    UIColor *t23BlueColor = [UIColor colorWithRed:53.0/255.0 green:149.0/255.0 blue:214.0/255.0 alpha:1.0];
+    [contactStudyButton setTitleColor:t23BlueColor forState:UIControlStateNormal];
+    contactStudyButton.titleLabel.font = [UIFont systemFontOfSize:16.0];
+    [textContentView addSubview:contactStudyButton];
+    contactStudyButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [contactStudyButton addConstraint:[NSLayoutConstraint constraintWithItem:contactStudyButton
+                                                                   attribute:NSLayoutAttributeHeight
+                                                                   relatedBy:NSLayoutRelationEqual
+                                                                      toItem:nil
+                                                                   attribute:NSLayoutAttributeNotAnAttribute
+                                                                  multiplier:1.0
+                                                                    constant:45.0]];
+    [textContentView addConstraint:[NSLayoutConstraint constraintWithItem:contactStudyButton
+                                                                attribute:NSLayoutAttributeTop
+                                                                relatedBy:NSLayoutRelationEqual
+                                                                   toItem:descriptionLabel
+                                                                attribute:NSLayoutAttributeBottom
+                                                               multiplier:1.0
+                                                                 constant:-10.0]];
+    [textContentView addConstraint:[NSLayoutConstraint constraintWithItem:contactStudyButton
+                                                                attribute:NSLayoutAttributeLeading
+                                                                relatedBy:NSLayoutRelationEqual
+                                                                   toItem:textContentView
+                                                                attribute:NSLayoutAttributeLeading
+                                                               multiplier:1.0
+                                                                 constant:15.0]];
+    [textContentView addConstraint:[NSLayoutConstraint constraintWithItem:contactStudyButton
                                                                 attribute:NSLayoutAttributeBottom
                                                                 relatedBy:NSLayoutRelationEqual
                                                                    toItem:textContentView
                                                                 attribute:NSLayoutAttributeBottom
                                                                multiplier:1.0
                                                                  constant:0.0]];
+    
     //--------------------
     // Try Again Button
     UIButton *tryAgainButton = [UIButton t23ButtonWithText:@"Try again" andHasBorder:YES];
