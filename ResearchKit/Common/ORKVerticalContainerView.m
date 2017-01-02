e534c9f34871484cf168e2e1503da126df25bf2c
Dariusz Lesniak <dariusz.lesniak@lifemap-solutions.com>
2017-01-02 16:40:46 +0100

    Xcode 8 slider's not usuable - fixing based on commit : https://github.com/ResearchKit/ResearchKit/pull/806/files

--- ResearchKit/Common/ORKVerticalContainerView.m
+++ ResearchKit/Common/ORKVerticalContainerView.m
@@ -573,7 +573,7 @@
                                                                            attribute:NSLayoutAttributeNotAnAttribute
                                                                           multiplier:1.0
                                                                             constant:ORKScreenMetricMaxDimension];
-        widthConstraint.priority = UILayoutPriorityFittingSizeLevel;
+        widthConstraint.priority = UILayoutPriorityDefaultHigh;
         [_variableConstraints addObject:widthConstraint];
         
         [_variableConstraints addObject:[NSLayoutConstraint constraintWithItem:_stepView
